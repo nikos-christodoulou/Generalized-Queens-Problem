259e36cc83b096b1cd0aee9e2190226cc1dc857a
nikos-christodoulou <nick.christodoulou11@gmail.com>
2021-11-15 17:25:43 +0200

    Complete Implementation by passing queens_number as a paremeter in GeneticAlgorithm constructor and in Chromosome run function.

--- Generalized_Queens/src/Main.java
+++ Generalized_Queens/src/Main.java
@@ -5,9 +5,10 @@ public class Main
     public static void main(String[] args)
     {
         //each time we change the GenereticAlgorithm's constructor input, as well as the first parameter of the Chromosome run function.
-        GeneticAlgorithm algorithm = new GeneticAlgorithm(4);
+        GeneticAlgorithm algorithm = new GeneticAlgorithm(10);
         //populationSize, mutationProbability, maximumSteps, minimumFitness
-        Chromosome solution = algorithm.run(4,1000, 0.08, 1000, 28);
+        Chromosome solution = algorithm.run(10,1000, 0.08, 1000, 28);
+        //TODO: Show the time needed for calculating the solution.
         solution.print();
     }
 }
